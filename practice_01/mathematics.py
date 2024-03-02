a6c28c4fa7ac35a2e591533a75c015f0260058b1
weilianlow <weilian.low88@gmail.com>
2024-03-02 16:01:01 +0800

    add example

---
+++ practice_01/mathematics.py
@@ -0,0 +1,11 @@
+class Mathematics:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def sum(cls, a, b):
+        return a + b
+
+    @classmethod
+    def divide(cls, a, b):
+        return a/b
